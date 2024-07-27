cfe831e8d7b33c86679b8749553ad1ae27f82a3f
Ezzz-Lui <ogn.lui@gmail.com>
2024-07-27 21:05:34 +0000

    Test: edit file routerIndex.js for testing routes

--- routes/routerIndex.js
+++ routes/routerIndex.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Test routing with express')
 })
 
 module.exports = router
